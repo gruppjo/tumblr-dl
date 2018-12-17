066d50f0dff17d8217ddd0730e202f7db1c1f0af
gruppjo <jona_blubb@web.de>
2018-12-17 06:44:40 +0100

    feat: download - async await and proper indices

--- download.js
+++ download.js
@@ -1,33 +1,44 @@
 const download = require('download');
 const allPhotos = require('./data-files/allPhotos.json');
+const allVideos = require('./data-files/allVideos.json');
+
+const OFFSET = 0;
+const CONCURRENT_ITEMS = 50;
 
 const downloadPromisesSet = async (promises) => {
   return Promise.all(promises)
   .then(() => {
     console.log(`downloaded set - ${index} of ${allPhotos.length}`);
-  });
+  })
 };
 
-let index = 0;
-const downloadPhotos = async () => {
-  while (index < allPhotos.length) {
+let index = OFFSET | 0; // can also be used as offset
 
-    const remainingPhotosCount = allPhotos.length - 1 - index;
-    const CONCURRENCY_COUNT = remainingPhotosCount >= 50 ? 50 : remainingPhotosCount;
+const downloadAll = async (allItems) => {
+  // index=3600 means the image with index 3600 will be downloaded next
+  // it doesn't mean it has already been downloaded
+  while (index < allItems.length) {
+
+    const remainingItemsCount = allItems.length - index;
+    const concurrencyCount = remainingItemsCount >= CONCURRENT_ITEMS ? CONCURRENT_ITEMS : remainingItemsCount;
 
     const promises = [];
-    for (let innerIndex = 0; innerIndex < CONCURRENCY_COUNT; innerIndex++) {
-      const photo = allPhotos[index + innerIndex];
-      promises.push(download(photo.url, `tumblr-files`, {
-        filename: photo.filename
+    for (let innerIndex = 0; innerIndex < concurrencyCount; innerIndex++) {
+      const item = allItems[index + innerIndex];
+      promises.push(download(item.url, `tumblr-files`, {
+        filename: item.filename
+      })
+      .catch((err) => {
+        console.log('ERROR', err && err.url);
       }));
-      console.log(`downloading ${photo.filename}`);
+      console.log(`downloading ${item.filename}`);
     }
 
     await downloadPromisesSet(promises);
 
-    index += CONCURRENCY_COUNT;
+    index += concurrencyCount; // add one more than the last downloaded index
   }
 };
 
-downloadPhotos();
+// downloadAll(allPhotos);
+downloadAll(allVideos);
