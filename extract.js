0a1af712bd1c0f67b54505a99f8a4fc23c77140b
gruppjo <jona_blubb@web.de>
2018-12-17 06:45:34 +0100

    feat: extract - extract videos (commit should have come before last commit)

--- extract.js
+++ extract.js
@@ -20,21 +20,39 @@ console.log('post types', JSON.stringify(types, null, 2));
 
 // extract data
 const allPhotos = [];
+const allVideos = [];
 posts.forEach((post) => {
 
   // photos
-  if (post.type === 'photo') {
-    post.photos.forEach((photo) => {
-      const url = photo.original_size.url;
-      const filename = `${post.liked_timestamp}-${url.match(/\w+\.\w+$/)}`;
-      const photoData = {
-        url,
-        filename
-      };
-      allPhotos.push(photoData);
-    });
+  // if (post.type === 'photo') {
+  //   post.photos.forEach((photo) => {
+  //     const url = photo.original_size.url;
+  //     const filename = `${post.liked_timestamp}-${url.match(/\w+\.\w+$/)}`;
+  //     const photoData = {
+  //       url,
+  //       filename
+  //     };
+  //     allPhotos.push(photoData);
+  //   });
+  // }
+
+  // videos
+  if (post.type === 'video') {
+    const url = post.video_url || ''; // apparently sometimes video url is missing
+    const filename = `${post.liked_timestamp}-${url.match(/\w+\.\w+$/)}`;
+    const videoData = {
+      url,
+      filename
+    };
+    allVideos.push(videoData);
   }
 
 });
-console.log(`extracted ${allPhotos.length} photos`);
-fs.writeFileSync('./data-files/allPhotos.json', JSON.stringify(allPhotos, null, 2));
+
+// write photos
+// console.log(`extracted ${allPhotos.length} photos`);
+// fs.writeFileSync('./data-files/allPhotos.json', JSON.stringify(allPhotos, null, 2));
+
+// write videos
+console.log(`extracted ${allVideos.length} videos`);
+fs.writeFileSync('./data-files/allVideos.json', JSON.stringify(allVideos, null, 2));
